ac6b37d7d76a823625358f6570c20a0cdf11cf20
bullet35-repo <earlconrad.bala@7.ustp.edu.ph>
2026-07-15 22:23:31 +0800

    implemented windows pop-up notification

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,7 +6,9 @@ export 'package:hrms_plaridel/app/app.dart' show MyApp;
 export 'package:hrms_plaridel/app/app_constants.dart' show kLoginAsKey;
 export 'package:hrms_plaridel/app/route_observer.dart' show routeObserver;
 
-Future<void> main() async {
-  final bootstrap = await bootstrapApp();
+Future<void> main(List<String> arguments) async {
+  final bootstrap = await bootstrapApp(
+    startHidden: arguments.contains('--hidden'),
+  );
   runApp(MyApp(auth: bootstrap.auth, themeNotifier: bootstrap.themeNotifier));
 }
--- frontend/windows/flutter/generated_plugins.cmake
+++ frontend/windows/flutter/generated_plugins.cmake
@@ -7,8 +7,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
   geolocator_windows
   printing
+  screen_retriever_windows
   share_plus
+  tray_manager
   url_launcher_windows
+  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
