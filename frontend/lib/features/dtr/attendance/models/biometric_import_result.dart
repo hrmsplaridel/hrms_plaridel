f95e992a4508e13c3de6d51d0a0dbeee2bded8e0
bullet35-repo <earlconrad.bala@7.ustp.edu.ph>
2026-09-17 21:08:38 +0800

    Implemented the inactive-employee attendance protection, right side panel and darkmode adaptable

--- frontend/lib/features/dtr/attendance/models/biometric_import_result.dart
+++ frontend/lib/features/dtr/attendance/models/biometric_import_result.dart
@@ -7,6 +7,7 @@ class BiometricImportResult {
     required this.duplicatesSkipped,
     required this.skippedUnmatched,
     required this.skippedIdentityMismatch,
+    required this.skippedOutsideEmployment,
     required this.skippedNoSchedule,
     required this.skippedHoliday,
     required this.skippedLeave,
@@ -24,6 +25,7 @@ class BiometricImportResult {
   final int duplicatesSkipped;
   final int skippedUnmatched;
   final int skippedIdentityMismatch;
+  final int skippedOutsideEmployment;
   final int skippedNoSchedule;
   final int skippedHoliday;
   final int skippedLeave;
@@ -44,6 +46,8 @@ class BiometricImportResult {
       skippedUnmatched: (json['skipped_unmatched'] as num?)?.toInt() ?? 0,
       skippedIdentityMismatch:
           (json['skipped_identity_mismatch'] as num?)?.toInt() ?? 0,
+      skippedOutsideEmployment:
+          (json['skipped_outside_employment'] as num?)?.toInt() ?? 0,
       skippedNoSchedule: (json['skipped_no_schedule'] as num?)?.toInt() ?? 0,
       skippedHoliday: (json['skipped_holiday'] as num?)?.toInt() ?? 0,
       skippedLeave: (json['skipped_leave'] as num?)?.toInt() ?? 0,
